a33dee06b9f29bad2e09b2d84931f8f5c4a4a624
1 <a1@1deMacBook-Pro.local>
2018-03-21 10:02:24 +0800

    feat:add GET but stream.fetch failel

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import {createElement, Component} from 'rax';
+import {createElement, Component, render} from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
+import ListView from 'rax-listview';
+import InfoService from './services';
 import styles from './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      data: []
+    }
+  }
+
+  componentWillMount() {
+    alert(window.location.href)
+    InfoService.getInfoList()
+      .then((data) => {
+        this.setData({data})
+      })
+  }
+
+  listItem = (item, index) => {
+    return (
+      <View>
+        <Text>{item.title}</Text>
+        <Text>{item.data}</Text>
+      </View>
+    );
+  }
   render() {
     return (
       <View style={styles.app}>
-        <View style={styles.appHeader}>
+        {/* <View style={styles.appHeader}>
           <Text style={styles.appBanner}>Welcome to Rax</Text>
         </View>
         <Text style={styles.appIntro}>
           To get started, edit src/App.js and save to reload.
-        </Text>
+        </Text> */}
+        <ListView
+          renderRow={this.listItem}
+          dataSource={this.state.data}
+        />
       </View>
     );
   }
